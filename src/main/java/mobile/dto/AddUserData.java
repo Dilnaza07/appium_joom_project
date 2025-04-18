952988248721f7099593cd3eef3b8b8f07ba8338
dilnaza07 <dilnazaturdumatova@gmail.com>
2025-04-18 18:54:47 +0600

    added basic methods and interfaces

---
+++ src/main/java/mobile/dto/AddUserData.java
@@ -0,0 +1,16 @@
+package mobile.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class AddUserData {
+
+    private String userFirstName;
+    private String userLastName;
+    private String userEmail;
+    private String userPassword;
+    private String userPasswordConfirmation;
+
+}

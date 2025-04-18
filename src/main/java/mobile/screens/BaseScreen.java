e8a99899b3a5942a3aa9058a336aff6be97100c3
dilnaza07 <dilnazaturdumatova@gmail.com>
2025-04-18 19:34:10 +0600

    added wait, tap, scroll methods

--- src/main/java/mobile/screens/BaseScreen.java
+++ src/main/java/mobile/screens/BaseScreen.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.PointerInput;
 import org.openqa.selenium.interactions.Sequence;
 import org.openqa.selenium.support.PageFactory;
-import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
@@ -24,7 +24,7 @@ public class BaseScreen {
 
     public void waitUtilElementIsVisible(WebElement element){
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(15));
-        wait.until(ExpectedCondition.visibilityOf(element));
+        wait.until(ExpectedConditions.visibilityOf(element));
     }
 
     public void tapByCoordinate(int x, int y){

16da922f117d6b29ab28d1faf3a8d6fac91a189d
dilnaza07 <dilnazaturdumatova@gmail.com>
2025-04-18 20:49:12 +0600

    creating the first autotest

--- src/test/java/tests/account/GetProfileScreenTest.java
+++ src/test/java/tests/account/GetProfileScreenTest.java
@@ -1,4 +1,24 @@
 package tests.account;
 
-public class GetProfileScreenTest {
+import mobile.helper.TestDescription;
+import mobile.screens.JoomHomeScreen;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import tests.BaseTest;
+
+import static mobile.enums.LoginTitle.LOGIN_TITLE;
+
+public class GetProfileScreenTest extends BaseTest {
+
+    @Test
+    @DisplayName("Open Login Screen")
+    @TestDescription("Close advirtisement, skip the quiz, open login screen")
+    public void testGetLoginScreen(){
+        JoomHomeScreen joomHomeScreen = new JoomHomeScreen(driver);
+        joomHomeScreen
+                .closeAdvertisement()
+                .skipQuiz()
+                .openLoginScreen()
+                .verifyLoginScreenOpened(LOGIN_TITLE);
+    }
 }
